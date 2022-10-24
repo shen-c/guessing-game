d671b8962659420511d182ad51c0e33cb06a24a4
shen-c <congshen@cpp.edu>
2022-10-23 20:13:08 -0700

    initial commit outline

---
+++ guessingGame.c
@@ -0,0 +1,13 @@
+//make function for game
+
+//make function for menu, validation, etc?
+
+int main(void){
+
+//display menu
+
+//loop thorugh options until user quits
+
+//display thank you message
+
+}
